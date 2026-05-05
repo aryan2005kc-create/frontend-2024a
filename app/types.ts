5292ad935a547227e1e4dc3d1d8d36517657e751
R E X Y <aryankc@R-E-X-Y-3.local>
2026-05-05 09:20:30 +0545

    frontend deployed

---
+++ app/types.ts
@@ -0,0 +1,11 @@
+export type QueryItem = {
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  query: string;
+};
+
+export const BACKEND_API_URL =
+  "https://cloud-app-gsbweqgsbzbvf3hp.uaenorth-01.azurewebsites.net/queries";
+
+export const APP_API_URL = "/api/queries";
